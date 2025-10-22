d32a7d7d668f457224daf4c97214e71222d2d1ea
tea-eraspillsthecode. <tierrageorge01@gmail.com>
2025-10-21 22:31:37 -0400

    integrated email JS

---
+++ app.js
@@ -0,0 +1,23 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_b234q2k",
+      "template_oc0ty7y",
+      event.target,
+      "GUiQGrSWJHOBQU7d4"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at tierrageorge01@gmail.com."
+      );
+    });
+}
